58e6c2afe8e38c3bdeefb284c296a33938cf551f
Masnun Siam <siam.cse.bsmrstu@gmail.com>
2023-07-15 22:34:41 +0600

    changed program name

--- install_windows.bat
+++ install_windows.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Install script for samftp.py on Windows
+REM Install script for samftp on Windows
 
 REM Step 1: Install Python if not already installed
 REM TODO: Add commands to check and install Python if needed
@@ -12,11 +12,11 @@ pip install --user pyfzf~=0.3.1
 
 REM Step 3: Copy the script to the desired location
 set DESTINATION_FOLDER=%APPDATA%\Python\Scripts
-copy "samftp.py" %DESTINATION_FOLDER%
+copy "samftp" %DESTINATION_FOLDER%
 
 REM Step 4: Add the shebang for Python 3 to the script
-echo #!%APPDATA%\Python\Python39\python.exe > %DESTINATION_FOLDER%\samftp.py.tmp
-type %DESTINATION_FOLDER%\samftp.py >> %DESTINATION_FOLDER%\samftp.py.tmp
-move /y %DESTINATION_FOLDER%\samftp.py.tmp %DESTINATION_FOLDER%\samftp.py
+echo #!%APPDATA%\Python\Python39\python.exe > %DESTINATION_FOLDER%\samftp.tmp
+type %DESTINATION_FOLDER%\samftp >> %DESTINATION_FOLDER%\samftp.tmp
+move /y %DESTINATION_FOLDER%\samftp.tmp %DESTINATION_FOLDER%\samftp
 
 echo Installation completed.
